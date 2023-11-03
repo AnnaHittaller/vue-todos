41aa7a7ac288c6a764c06c13c1b4757bb411a338
Anna Hittaller <70811887+AnnaHittaller@users.noreply.github.com>
2023-11-03 07:57:57 +0000

    about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Vue Todos</h1>
+    <p>Vue Todos is a simple CRUD-based todo app, built based on the tutorial of John Komarnicki.</p>
+    <p>You can add, save, delete and edit todos, which are saved to local storage.</p>
+    <p>Some additional functions may be added to the app later to enhance user experience.</p>
   </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.about {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
 
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  p{
+    text-align: center;
+    margin: 12px 0;
+  }
+}
 </style>
